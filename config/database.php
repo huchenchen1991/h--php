322ffccd6b9896fc23277808379266a942956f56
huchenchen <363429664@qq.com>
2019-04-11 14:27:59 +0800

    第一次上传

--- config/database.php
+++ config/database.php
@@ -2,11 +2,11 @@
 /**
 *数据库连接配置
 */
-//本地localhost,h2数据库
+//本地localhost,h数据库
 $db['default']['host'] = "localhost";
 $db['default']['username'] = 'root';
 $db['default']['password'] = '123456';
-$db['default']['dbname'] = 'h2';
+$db['default']['dbname'] = 'h';
 
 //
 // $db['bd2']['host'] = "";
